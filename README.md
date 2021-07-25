9c8495f7ecabef387b9093b60ac415b591be4144
Srimaani <72712974+Srimaani@users.noreply.github.com>
2021-07-25 18:22:15 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# game-state
+# C37RV_SpeedRacer_StudentActivity
+Student Activity
---
+++ js/Game.js
@@ -0,0 +1,30 @@
+class Game {
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+update(gamestate) {
+  database.ref("/").update({
+    gamestate:state
+  })
+}
+  play() {}
+}
